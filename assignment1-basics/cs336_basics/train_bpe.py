bc33ff19e0b6c5682b1181f947223fc983682cb3
Liaoyi Xu <lx2755@kirkcomp01.ccbb.utexas.edu>
2026-07-08 21:44:41 -0500

    optimize bpe training

--- assignment1-basics/cs336_basics/train_bpe.py
+++ assignment1-basics/cs336_basics/train_bpe.py
@@ -4,10 +4,11 @@ import os
 import pickle
 import time
 
-from collections import Counter
+from collections import Counter, defaultdict
 from multiprocessing import Pool
 from typing import BinaryIO, List, Tuple, Dict
 from functools import partial
+from tqdm import tqdm
 
 
 def process_chunk(text, special_tokens):
@@ -81,7 +82,7 @@ def train_bpe(
     # split text
     chunks = []
     with open(input_path, "rb") as f:
-        num_processes = 100
+        num_processes = 8
         boundaries = find_chunk_boundaries(f, num_processes, special_tokens[0].encode("utf-8"))
         for start, end in zip(boundaries[:-1], boundaries[1:]):
             # print(start, end)
@@ -102,9 +103,10 @@ def train_bpe(
     all_counter = Counter()
     for c in counters:
         all_counter.update(c)
-        # for token, count in c.items():
-        #     all_counter[tuple(bytes([b]) for b in token)] += count
     
+    token_states = list(all_counter.keys())
+    token_counts = list(all_counter.values())
+
     # train bpe
     vocab = {}
 
@@ -119,30 +121,40 @@ def train_bpe(
         
     merges = []
 
-    while next_id < vocab_size:
-        # log progress
-        if next_id % 1000 == 0:
-            print(f"Vocab size: {next_id}")
-            
-        # one merge
-        bcounter = Counter()
-        for token, count in all_counter.items():
-            for j in range(len(token) - 1):
-                bcounter[(token[j], token[j + 1])] += count
+    pair_to_tokens = defaultdict(set)
+    pair_counts = Counter()
+    for token_id, (token, count) in enumerate(all_counter.items()):
+        for j in range(len(token) - 1):
+            pair_to_tokens[(token[j], token[j + 1])].add(token_id)
+            pair_counts[(token[j], token[j + 1])] += count
 
-        max_count = max(bcounter.values())
-        keys = [k for k, v in bcounter.items() if v == max_count]
+    # tqdm
+    pbar = tqdm(total=vocab_size - next_id, desc="Learning BPE merges")
+    while next_id < vocab_size:
+        max_count = max(pair_counts.values())
+        keys = [k for k, v in pair_counts.items() if v == max_count]
         max_key = max(keys)
 
         vocab[next_id] = max_key[0] + max_key[1]
         merges.append(max_key)
         next_id += 1
 
-        new_all_counter = Counter()
-        for token, count in all_counter.items():
+        token_ids = pair_to_tokens[max_key].copy()
+        for token_id in token_ids:
+            token = token_states[token_id]
+
+            # remove adjacent pair
+            for pair in zip(token[:-1], token[1:]):
+                pair_counts[pair] -= token_counts[token_id]
+                if pair_counts[pair] == 0:
+                    pair_counts.pop(pair)
+            for pair in set(zip(token[:-1], token[1:])):    
+                pair_to_tokens[pair].remove(token_id)
+                if pair_to_tokens[pair] == set():
+                    pair_to_tokens.pop(pair)
+
             new_token = []
             j = 0
-
             while j < len(token):
                 if j < len(token) - 1 and (token[j], token[j + 1]) == max_key:
                     new_token.append(token[j] + token[j + 1])
@@ -150,8 +162,14 @@ def train_bpe(
                 else:
                     new_token.append(token[j])
                     j += 1
-            new_all_counter[tuple(new_token)] += count
-        all_counter = new_all_counter
+            token_states[token_id] = tuple(new_token)
+
+            for j in range(len(new_token) - 1):
+                pair_to_tokens[(new_token[j], new_token[j + 1])].add(token_id)
+                pair_counts[(new_token[j], new_token[j + 1])] += token_counts[token_id]
+
+        pbar.update(1)
+    pbar.close()
 
     return vocab, merges
 
@@ -160,29 +178,35 @@ if __name__ == "__main__":
     import json
 
     # tiny stories
-    # vocab, merges = train_bpe(
-    #     input_path="../data/TinyStoriesV2-GPT4-train.txt",
-    #     vocab_size=10000,
-    #     special_tokens=['<|endoftext|>']
-    # )
+    print("**training BPE for tiny stories")
+    vocab, merges = train_bpe(
+        input_path="../data/TinyStoriesV2-GPT4-train.txt",
+        vocab_size=10000,
+        special_tokens=['<|endoftext|>']
+    )
 
-    # json_vocab = {
-    #     str(k): list(v)
-    #     for k, v in vocab.items()
-    # }
+    json_vocab = {
+        str(k): list(v)
+        for k, v in vocab.items()
+    }
 
-    # json_merges = [
-    #     [list(a), list(b)]
-    #     for a, b in merges
-    # ]
+    json_merges = [
+        [list(a), list(b)]
+        for a, b in merges
+    ]
 
-    # with open("./tiny_stories_vocab.json", "w") as f:
-    #     json.dump(json_vocab, f)
+    with open("./tiny_stories_vocab.json", "w") as f:
+        json.dump(json_vocab, f)
 
-    # with open("./tiny_stories_merges.json", "w") as f:
-    #     json.dump(json_merges, f)
+    with open("./tiny_stories_merges.json", "w") as f:
+        json.dump(json_merges, f)
+
+    print(f"tiny stories vocab saved to ./tiny_stories_merges.json")
+    print(f"tiny stories merges saved to ./tiny_stories_merges.json")
+    print()
 
     # open web text
+    print("**training BPE for open web text")
     vocab, merges = train_bpe(
         input_path="../data/owt_train.txt",
         vocab_size=32000,
@@ -204,3 +228,7 @@ if __name__ == "__main__":
 
     with open("./owt_merges.json", "w") as f:
         json.dump(json_merges, f)
+
+    print(f"open web text vocab saved to ./tiny_stories_merges.json")
+    print(f"open web text merges saved to ./tiny_stories_merges.json")
+    print()
